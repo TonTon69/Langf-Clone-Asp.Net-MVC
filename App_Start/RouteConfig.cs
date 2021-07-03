baea1b48e2e694b1e4e2925b21b1cbbfd5313949
TonTon69 <hoanghuynh0609@gmail.com>
2021-07-03 22:00:43 +0700

    Add project files.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace langfvn
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "home", action = "index", id = UrlParameter.Optional },
+                namespaces: new[] { "langfvn.Controllers" }
+            );
+        }
+    }
+}
---
+++ Areas/admin/Controllers/homeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace langfvn.Areas.admin.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: admin/home
+        public ActionResult index()
+        {
+            return View();
+        }
+    }
+}
