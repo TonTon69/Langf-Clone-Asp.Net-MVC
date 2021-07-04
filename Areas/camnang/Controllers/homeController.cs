2060aa17735a89c2f9db4e532913950d77d009b3
TonTon69 <hoanghuynh0609@gmail.com>
2021-07-04 11:59:00 +0700

    area camnang

---
+++ Areas/camnang/Controllers/homeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace langfvn.Areas.camnang.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: camnang/home
+        public ActionResult index()
+        {
+            return View();
+        }
+    }
+}
