89ac922dc375a9817775582a4c21031e2ef6fc18
hunterfairy99@icloud.com <phucan332000@gmail.com>
2021-07-04 13:20:41 +0700

    update test

---
+++ Areas/camnang/camnangAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace langfvn.Areas.camnang
+{
+    public class camnangAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "camnang";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "camnang_default",
+                "camnang/{controller}/{action}/{id}",
+                new { action = "index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
