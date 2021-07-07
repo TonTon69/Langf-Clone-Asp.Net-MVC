c299878067332dd4bf35f82e0e35aef1cb95644e
TonTon69 <hoanghuynh0609@gmail.com>
2021-07-07 16:22:55 +0700

    manager reviews

--- Areas/admin/Controllers/ReviewsController.cs
+++ Areas/admin/Controllers/ReviewsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using langfvn.Models;
+using PagedList;
 
 namespace langfvn.Areas.admin.Controllers
 {
@@ -15,68 +16,23 @@ namespace langfvn.Areas.admin.Controllers
         private LangfvnContext db = new LangfvnContext();
 
         // GET: admin/Reviews
-        public ActionResult Index()
+        public ActionResult Index(int? page, string search)
         {
-            var reviews = db.Reviews.Include(r => r.Account).Include(r => r.Store);
-            return View(reviews.ToList());
-        }
-
-        // GET: admin/Reviews/Create
-        public ActionResult Create()
-        {
-            ViewBag.UserID = new SelectList(db.Accounts, "UserID", "FullName");
-            ViewBag.StoreID = new SelectList(db.Stores, "StoreID", "StoreName");
-            return View();
-        }
-
-        // POST: admin/Reviews/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ReviewID,UserID,StoreID,Content,Image,Star")] Review review)
-        {
-            if (ModelState.IsValid)
+            if (TempData["success"] != null)
             {
-                db.Reviews.Add(review);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                ViewBag.SuccessMsg = TempData["success"];
             }
-
-            ViewBag.UserID = new SelectList(db.Accounts, "UserID", "FullName", review.UserID);
-            ViewBag.StoreID = new SelectList(db.Stores, "StoreID", "StoreName", review.StoreID);
-            return View(review);
-        }
-
-        // GET: admin/Reviews/Edit/id
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
+            ViewBag.CurrentFilter = search;
+            var reviews = from r in db.Reviews select r;
+            if (!string.IsNullOrEmpty(search))
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                reviews = reviews.Where(s => s.Account.FullName.Contains(search) ||
+                s.Content.Contains(search) || s.Store.StoreName.Contains(search));
             }
-            Review review = db.Reviews.Find(id);
-            if (review == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.UserID = new SelectList(db.Accounts, "UserID", "FullName", review.UserID);
-            ViewBag.StoreID = new SelectList(db.Stores, "StoreID", "StoreName", review.StoreID);
-            return View(review);
-        }
-
-        // POST: admin/Reviews/Edit/id
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ReviewID,UserID,StoreID,Content,Image,Star")] Review review)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(review).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.UserID = new SelectList(db.Accounts, "UserID", "FullName", review.UserID);
-            ViewBag.StoreID = new SelectList(db.Stores, "StoreID", "StoreName", review.StoreID);
-            return View(review);
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+            reviews = reviews.OrderBy(s => s.ReviewID);
+            return View(reviews.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: admin/Reviews/Delete/id
